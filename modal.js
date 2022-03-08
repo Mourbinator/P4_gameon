ea4253b038fc28a9355b063c84a5e60a0b041e6d
mourbin <jipeg972@gmail.com>
2022-03-08 01:07:28 +0100

    error_file1

--- modal.js
+++ modal.js
@@ -17,18 +17,24 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
+  alert("ok");
   modalbg.style.display = "block";
 }
 
-const fermemodalbtn = document.querySelectorAll('#close');
-fermemodalbtn.addEventListener("click",fermermodal){
-  
-function fermermodal
-{
-  modal-body.style.display = "none";
-}
-});
+const fermemodalbtn = document.querySelector(".close");
+fermemodalbtn.addEventListener("click",fermermodal);
 
+let modal1 =  document.querySelector('.bground');
+console.log(modal1);
+
+function fermermodal()
+{
+ modal1.style.display = "none";
+}
+
+// Balise formulaire
+const formulaire = document.forms["reserve"];
+const nom1 = document.getElementById("last");
 const prenom = document.getElementById("first");
 const email = document.getElementById("email");
 const datenaissance = document.getElementById("birthdate");
@@ -40,25 +46,103 @@ const boston = document.getElementById("location5");
 const portland = document.getElementById("location6");
 const tournoi = document.getElementById("quantity");
 
+//regex
+const regname = /^[a-zA-Z-\s]+$/;
+const regmail = /^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+const regbirth = /^((19[3-9]+[0-9]|200[0-9])-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])|(0?[1-9]|[12]\d|3[01])[/](0?[1-9]|1[0-2])[/](19[3-9]+[0-9]|200[0-6]))$/;
+
+
+//mise en place formulaire
+
+
+formulaire.addEventListener("submit",submit);
+
+function submit(e)
+{
+  e.preventDefault();
+
+
+/*
+  if(prenom.value.length <2 ||regname.test(prenom.value) == false)
+  {
+    errormsg(prenom);
+  } else {
+    errormsghide(prenom);
+    prenom.innerText = event.target.value;
+  }
+  */
+
+}
+
+
 //nom à 3 lettre minimum
 
-const nom1 = document.getElementById("last");
-nom1.addEventListener("input",function(troisletter);
 
 
-  function troisletter(event)
+  prenom.addEventListener("input",firstletter);
+  function firstletter(event)
   {
-    if(event.target.value<3){
-      console.log("veuillez entrer au moins 3 lettres pour le nom")
+    if(event.target.value.length<=2 || regname.test(event.target.value) == false){
+      errormsg(prenom);
+       /*formData.style.display = "block";*/
     } else{
-      nom1.innerText = event.target.value;
+      errormsghide(prenom);
+      //prenom.innerText = event.target.value;
     }
   }
+
+  nom1.addEventListener("input",lastletter);
+  function lastletter(event)
+  {
+    if(event.target.value.length<=2 || regname.test(event.target.value) == false{
+      errormsg(nom1);
+       /*formData.style.display = "block";*/
+    } else{
+      errormsghide(nom1);
+      //nom1.innerText = event.target.value;
+    }
+  }
+
+  //vérif email
+  if(regmail.test(email.value))
+  {
+    errormsghide(email);
+  }else{
+    errormsg(email);
+  }
+
+  //vérid date de naissance
+  if(regbirth.test(datenaissance.value))
+  {
+    errormsghide(datenaissance);
+  }else{
+    errormsg(datenaissance);
+  }
+
+  //vérif tournois
+  
+
+
+
+
+
+
+
+
+
+function errormsg(elt){
+  elt.closest(".formData").dataset.errorVisible = "true";
+}
+function errormsghide(elt){
+  elt.closest(".formData").dataset.errorVisible = "false";
+}
 
 
 /*
   const prenom = document.getElementById("first");
-  prenom.addEventListener("input",function(troisletter);
+  <data-error="Firstname must not be empty."
+  data-error-visible="false"
+
 */
 
 /*
